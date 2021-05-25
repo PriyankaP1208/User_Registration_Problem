711136335b52e92a87f041f96bafefcd7c6123b6
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-25 23:27:27 +0530

    ADDED:Functionality to validate last name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -6,17 +6,27 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args)
     {
-        String fname;
+        String firstName,lastName;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter first Name:");
-        fname=sc.nextLine();
-        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",fname))
+        firstName=sc.nextLine();
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName))
         {
-            System.out.println("Valid First Name");
+            System.out.println("Valid first Name");
         }
         else
         {
-            System.out.println("Invalid First Name");
+            System.out.println("Invalid first Name");
+        }
+        System.out.println("Enter Last name");
+        lastName = sc.nextLine();
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",lastName))
+        {
+            System.out.println("Valid last Name");
+        }
+        else
+        {
+            System.out.println("Invalid last Name");
         }
     }
 }
