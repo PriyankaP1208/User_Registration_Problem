68ffb03d6b10a8e30aef0c5ee93032755ee5b5d3
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-25 22:02:33 +0530

    ADDED:Functionality to validate first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,5 +1,23 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    public static void main(String[] args)
+    {
+        String fname;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first Name:");
+        fname=sc.nextLine();
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",fname))
+        {
+            System.out.println("Valid First Name");
+        }
+        else
+        {
+            System.out.println("Invalid First Name");
+        }
+    }
 
 }
