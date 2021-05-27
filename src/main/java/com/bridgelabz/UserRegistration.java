d6e0a24884ae4df33c4dbb8adc23c23a613554e0
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-27 16:25:06 +0530

    ADDED:Refactored code

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -8,9 +8,12 @@ public class UserRegistration {
     {
         String firstName,lastName,email,mobile,password;
         Scanner sc=new Scanner(System.in);
+        String namePattern = "^[A-Z]{1}[a-z]{2,}$";
+
+        //Validate first name
         System.out.println("Enter first Name:");
-        firstName=sc.nextLine();
-        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName))
+        firstName = sc.nextLine();
+        if(Pattern.matches(namePattern, firstName))
         {
             System.out.println("Valid first Name");
         }
@@ -18,9 +21,11 @@ public class UserRegistration {
         {
             System.out.println("Invalid first Name");
         }
-        System.out.println("Enter Last name");
+
+        //Validate last name
+        System.out.println("Enter Last name:");
         lastName = sc.nextLine();
-        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}",lastName))
+        if(Pattern.matches(namePattern, lastName))
         {
             System.out.println("Valid last Name");
         }
@@ -28,9 +33,11 @@ public class UserRegistration {
         {
             System.out.println("Invalid last Name");
         }
+
+        //Validate email
         System.out.println("Enter Email:");
-        email=sc.nextLine();
-        if(Pattern.matches("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$",email))
+        email = sc.nextLine();
+        if(Pattern.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$", email))
         {
             System.out.println("Valid Email");
         }
@@ -38,9 +45,11 @@ public class UserRegistration {
         {
             System.out.println("Invalid Email");
         }
+
+        //Validate mobile number
         System.out.println("Enter Mobile:");
-        mobile=sc.nextLine();
-        if(Pattern.matches("^[0-9]{2} [0-9]{10}",mobile))
+        mobile = sc.nextLine();
+        if(Pattern.matches("^[0-9]{2} [0-9]{10}$", mobile))
         {
             System.out.println("Valid Mobile Number");
         }
@@ -48,9 +57,11 @@ public class UserRegistration {
         {
             System.out.println("Invalid Mobile Number");
         }
+
+        //Validate password
         System.out.println("Enter Password:");
-        password=sc.nextLine();
-        if(Pattern.matches("^(?=.*[A-Z])"+"(?=.*[0-9]).{8,32}$",password))
+        password = sc.nextLine();
+        if(Pattern.matches("^(?=.[0-9])"+"(?=.[a-z])(?=.[A-Z])"+"(?=.[@#$%^&+=]).{8,20}$", password))
         {
             System.out.println("Valid Password");
         }
