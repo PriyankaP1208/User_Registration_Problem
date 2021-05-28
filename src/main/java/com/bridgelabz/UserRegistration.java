1575e14f86d029ada8cc5f0ae5f3651827e5739e
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-28 22:17:37 +0530

    ADDED:JUnit Test to validate the user entry

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,73 +1,85 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main(String[] args)
-    {
-        String firstName,lastName,email,mobile,password;
-        Scanner sc=new Scanner(System.in);
-        String namePattern = "^[A-Z]{1}[a-z]{2,}$";
+    String namePatter = "^[A-Z]{1}[a-z]{2,}$";
+    String emailPattern = "^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$";
+    String mobilePattern = "^[0-9]{2} [0-9]{10}$";
+    String passwordPattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,32}$";
 
-        //Validate first name
-        System.out.println("Enter first Name:");
-        firstName = sc.nextLine();
-        if(Pattern.matches(namePattern, firstName))
+    //Validate first name
+    public boolean validFirstName(String fname)
+    {
+        if(Pattern.matches(namePatter, fname))
         {
-            System.out.println("Valid first Name");
+            System.out.println("Valid First Name");
+            return true;
         }
         else
         {
-            System.out.println("Invalid first Name");
+            System.out.println("Invalid First Name");
+            return false;
         }
+    }
 
-        //Validate last name
-        System.out.println("Enter Last name:");
-        lastName = sc.nextLine();
-        if(Pattern.matches(namePattern, lastName))
+    //Validate last name
+    public boolean validLastName(String lname)
+    {
+        if(Pattern.matches(namePatter, lname))
         {
             System.out.println("Valid last Name");
+            return true;
         }
         else
         {
             System.out.println("Invalid last Name");
+            return false;
         }
+    }
 
-        //Validate email
-        System.out.println("Enter Email:");
-        email = sc.nextLine();
-        if(Pattern.matches("^[a-zA-Z0-9]+([.][a-zA-Z0-9]+)?@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$", email))
+    //Validate email address
+    public boolean validEmail(String email){
+        if(Pattern.matches(emailPattern, email))
         {
-            System.out.println("Valid Email");
+            System.out.println("Valid email");
+            return true;
         }
         else
         {
-            System.out.println("Invalid Email");
+            System.out.println("Invalid email");
+            return false;
         }
+    }
 
-        //Validate mobile number
-        System.out.println("Enter Mobile:");
-        mobile = sc.nextLine();
-        if(Pattern.matches("^[0-9]{2} [0-9]{10}$", mobile))
+    //Validate phone number
+    public boolean validMobile(String phoneNo){
+        if(Pattern.matches(mobilePattern, phoneNo))
         {
-            System.out.println("Valid Mobile Number");
+            System.out.println("Valid mobile number");
+            return true;
         }
         else
         {
-            System.out.println("Invalid Mobile Number");
+            System.out.println("Invalid mobile number");
+            return false;
         }
+    }
 
-        //Validate password
-        System.out.println("Enter Password:");
-        password = sc.nextLine();
-        if(Pattern.matches("^(?=.[0-9])"+"(?=.[a-z])(?=.[A-Z])"+"(?=.[@#$%^&+=]).{8,20}$", password))
+    //validate password
+    public boolean validPassword(String password){
+        if(Pattern.matches(passwordPattern, password))
         {
             System.out.println("Valid Password");
+            return true;
         }
         else
         {
             System.out.println("Invalid Password");
+            return false;
         }
     }
 }
+
