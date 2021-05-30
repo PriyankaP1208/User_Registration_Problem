64d4be834152d2adc5f325f4a1682f47ee4b206e
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-05-30 23:43:26 +0530

    ADDED:Refactored parameterised code

---
+++ src/test/java/com/bridgelabz/ValidEmailTest.java
@@ -0,0 +1,35 @@
+package com.bridgelabz;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class ValidEmailTest {
+    public String email;
+    public boolean status;
+
+    public ValidEmailTest(String email, boolean status) {
+        this.email = email;
+        this.status = status;
+    }
+
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true},
+                {"abc.100@yahoo.com", true}, {"abc111@abc.com", true}, {"abc", false}, {"abc@.com.my", false},
+                {"abc123@gmail.a", false}, {"abc123@.com", false}, {"abc123@.com.com", false},
+                {"abc..2002@gmail.com", false}, {"abc.@gmail.com", false}, {"abc@abc@gmail.com", false},
+                {"abc@gmail.com.1a", false}, {"abc@gmail.com.aa.au", false}});
+    }
+
+    @Test
+    public void givenEmail_ShouldPassTestCases() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean result = userRegistration.parameterizedEmail(this.email, this.status);
+        assertTrue(result);
+    }
+
+}
