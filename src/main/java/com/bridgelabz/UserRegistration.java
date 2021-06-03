3a9bb7e9d17b95f838531a118f086c920dbb246f
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-06-03 13:17:16 +0530

    ADDED:functionality using lambda function

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -11,105 +12,98 @@ public class UserRegistration {
     String passwordPattern = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,32}$";
 
     //Validate first name
-    public boolean validFirstName(String fname) throws UserRegistrationException
-    {
+    public boolean validFirstName(String fname) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(namePatter);
         try {
-            if (Pattern.matches(namePatter, fname)) {
+            if (check.test(fname)) {
                 System.out.println("Valid First Name");
                 return true;
             } else {
                 throw new UserRegistrationException("Invalid first name");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid first name");
         }
     }
 
     //Validate last name
-    public boolean validLastName(String lname) throws UserRegistrationException
-    {
+    public boolean validLastName(String lname) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(namePatter);
         try {
-            if (Pattern.matches(namePatter, lname)) {
+            if (check.test(lname)) {
                 System.out.println("Valid last Name");
                 return true;
             } else {
                 throw new UserRegistrationException("Invalid last name");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid last name");
         }
     }
 
     //Validate email address
-    public boolean validEmail(String email)throws UserRegistrationException
-    {
+    public boolean validEmail(String email) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(emailPattern);
         try {
-            if (Pattern.matches(emailPattern, email)) {
+            if (check.test(email)) {
                 System.out.println("Valid email");
                 return true;
             } else {
                 throw new UserRegistrationException("Invalid email");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid email");
         }
     }
 
     //Validate phone number
     public boolean validMobile(String phoneNo) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(mobilePattern);
         try {
-            if (Pattern.matches(mobilePattern, phoneNo)) {
+            if (check.test(phoneNo)) {
                 System.out.println("Valid mobile number");
                 return true;
             } else {
                 throw new UserRegistrationException("Invalid mobile number");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid mobile number");
         }
     }
 
     //validate password
     public boolean validPassword(String password) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(passwordPattern);
         try {
-            if (Pattern.matches(passwordPattern, password)) {
+            if (check.test(password)) {
                 System.out.println("Valid Password");
                 return true;
             } else {
                 throw new UserRegistrationException("Invalid password");
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid password");
         }
     }
-    public boolean parameterizedEmail(String email, boolean status) throws UserRegistrationException
-    {
+
+    public boolean parameterizedEmail(String email, boolean status) throws UserRegistrationException {
+        Predicate<String> check = n -> n.matches(emailPattern);
         try {
             boolean returnVal;
-            if (Pattern.matches(emailPattern, email)) {
-                System.out.println("Valid email");
-                returnVal = true;
+            if (check.test(email))
+            {
+                System.out.println("Valid email");returnVal = true;
             } else {
-                throw new UserRegistrationException("Invalid email");
+                System.out.println("Invalid email");
+                returnVal = false;
             }
             if (status == returnVal)
                 return true;
             return false;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new UserRegistrationException("Invalid email");
         }
     }
 }
+
 
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -7,14 +7,14 @@ import org.junit.runner.JUnitCore;
 public class UserRegistrationTest {
     //Testing first name
     @Test
-    public void givenFirstName_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
+    public void givenFirstName_WhenValid_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validFirstName("Priyanka");
         assertTrue(result);
     }
 
     @Test
-    public void givenFirstName_WhenShort_ShouldReturnFalse() throws UserRegistrationException {
+    public void givenFirstName_WhenInvalid_ShouldReturnFalse() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validFirstName("pri");
         assertFalse(result);
@@ -22,14 +22,14 @@ public class UserRegistrationTest {
 
     //Testing last name
     @Test
-    public void givenLastName_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
+    public void givenLastName_WhenValid_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validLastName("Patil");
         assertTrue(result);
     }
 
     @Test
-    public void givenLastName_WhenShort_ShouldReturnFalse() throws UserRegistrationException {
+    public void givenLastName_WhenInvalid_ShouldReturnFalse() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validLastName("pl");
         assertFalse(result);
@@ -37,14 +37,14 @@ public class UserRegistrationTest {
 
     //Testing email address
     @Test
-    public void givenEmail_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
+    public void givenEmail_WhenValid_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validEmail("abc@yahoo.com");
         assertTrue(result);
     }
 
     @Test
-    public void givenEmail_WhenShort_ShouldReturnFalse() throws UserRegistrationException {
+    public void givenEmail_WhenInvalid_ShouldReturnFalse() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validEmail("abc.xyz.co.in");
         assertFalse(result);
@@ -52,14 +52,14 @@ public class UserRegistrationTest {
 
     //Testing phone number
     @Test
-    public void givenMobileNumber_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
+    public void givenMobileNumber_WhenValid_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validMobile("91 9881203930");
         assertTrue(result);
     }
 
     @Test
-    public void givenMobileNumber_WhenShort_ShouldReturnFalse() throws UserRegistrationException {
+    public void givenMobileNumber_WhenInvalid_ShouldReturnFalse() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validMobile("988120393");
         assertFalse(result);
@@ -67,14 +67,14 @@ public class UserRegistrationTest {
 
     //Testing password
     @Test
-    public void givenPassword_WhenProper_ShouldReturnTrue() throws UserRegistrationException {
+    public void givenPassword_WhenValid_ShouldReturnTrue() throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validPassword("Priyanka@123");
         assertTrue(result);
     }
 
     @Test
-    public void givenPassword_WhenShort_ShouldReturnFalse()throws UserRegistrationException {
+    public void givenPassword_WhenInvalid_ShouldReturnFalse()throws UserRegistrationException {
         UserRegistration userRegistration = new UserRegistration();
         boolean result = userRegistration.validPassword("priya12");
         assertFalse(result);
